21c11692f69b8b3d075071d1670b193d8c2888ea
MayOnesita <may.oropeza01@gmail.com>
2024-02-19 23:45:51 -0400

    game grid added + clock and more resources

--- minesweeper.lua
+++ minesweeper.lua
@@ -1,130 +1,207 @@
 -----------------------------------------------------------------------------------------
---
 -- minesweeper.lua
---
 -----------------------------------------------------------------------------------------
 
 local composer = require( "composer" )
 local scene = composer.newScene()
 local resources_folder = "resources/"
 
--- Colors
 local sup_bar_color = {0.3647,0.2509,0.2156,0.7}
-
--- include Corona's "physics" library
--- local physics = require "physics"
 local widget = require( "widget" )
 
+-----------------------------------------------------------------------------------------
 
---------------------------------------------
-
--- forward declarations and other locals
+-- Screen Variables
 local screenW, screenH, halfW = display.actualContentWidth, display.actualContentHeight, display.contentCenterX
 
-function scene:create( event )
+-- Main variables
+local grid = {} -- 2D array to hold our grid
+local cellSize = 30 
+local gridRows = 10 
+local gridColumns = 10
 
-	-- Called when the scene's view does not exist.
-	-- 
-	-- INSERT code here to initialize the scene
-	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
+local nBombs = 10
 
-	local sceneGroup = self.view
-
-	-- We need physics started to add bodies, but we don't want the simulaton
-	-- running until the scene is on the screen.
-	-- physics.start()
-	-- physics.pause()
-
-
-	-- create a grey rectangle as the backdrop
-	-- the physical screen will likely be a different shape than our defined content area
-	-- since we are going to position the background from it's top, left corner, draw the
-	-- background at the real top, left corner.
-
-	-- local background = display.newRect( display.screenOriginX, display.screenOriginY, screenW, screenH )
-	-- background.anchorX = 0 
-	-- background.anchorY = 0
-	-- background:setFillColor( .5 )
-
-	local background = display.newImageRect(resources_folder.."game_bg.png", screenH+200, screenH)
-	background.anchorX = 0.5
-	background.anchorY = 0.5
-	background.x = display.contentCenterX
-	background.y = display.contentCenterY
-
-	local game_supbar = display.newRect( display.screenOriginX, display.screenOriginY, screenW, screenH/5 )
-	game_supbar.anchorX = 0
-	game_supbar.anchorY = 0
-	game_supbar:setFillColor(unpack(sup_bar_color))
-	
-	-- -- make a crate (off-screen), position it, and rotate slightly
-	-- local crate = display.newImageRect( "crate.png", 90, 90 )
-	-- crate.x, crate.y = 160, -100
-	-- crate.rotation = 15
-	
-	-- -- add physics to the crate
-	-- physics.addBody( crate, { density=1.0, friction=0.3, bounce=0.3 } )
-	
-	-- -- create a grass object and add physics (with custom shape)
-	-- local grass = display.newImageRect( "grass.png", screenW, 82 )
-	-- grass.anchorX = 0
-	-- grass.anchorY = 1
-	-- --  draw the grass at the very bottom of the screen
-	-- grass.x, grass.y = display.screenOriginX, display.actualContentHeight + display.screenOriginY
-	
-	-- -- define a shape that's slightly shorter than image bounds (set draw mode to "hybrid" or "debug" to see)
-	-- local grassShape = { -halfW,-34, halfW,-34, halfW,34, -halfW,34 }
-	-- physics.addBody( grass, "static", { friction=0.3, shape=grassShape } )
-	
-	-- all display objects must be inserted into group
-	sceneGroup:insert( background )
-	sceneGroup:insert( game_supbar )
-	-- sceneGroup:insert( crate )
+-- Function to create the game grid, 2D array to represent mathematically the grid
+local function createGameGrid()
+    local gameGrid = {} 
+    for i = 1, gridRows do
+        gameGrid[i] = {} 
+        for j = 1, gridColumns do
+            gameGrid[i][j] = -1 
+        end
+    end
+    return gameGrid
 end
 
+-- Function to set random bombs in the grid
+local function setBombs(gameGrid, nBombs)
+	local bombs = 0
+	while bombs < nBombs do
+		local i = math.random(gridRows)
+		local j = math.random(gridColumns)
+		gameGrid[i][j] = 0
+		bombs = bombs + 1
+	end
+end
+
+-- Function to fill the grid with the number of bombs around each cell
+local function fillGrid(gameGrid)
+	for i = 1, gridRows do
+		for j = 1, gridColumns do
+			if gameGrid[i][j] == 0 then
+				if i > 1 then
+					if gameGrid[i-1][j] ~= 0 then
+						gameGrid[i-1][j] = gameGrid[i-1][j] + 1
+					end
+					if j > 1 then
+						if gameGrid[i-1][j-1] ~= 0 then
+							gameGrid[i-1][j-1] = gameGrid[i-1][j-1] + 1
+						end
+					end
+					if j < gridColumns then
+						if gameGrid[i-1][j+1] ~= 0 then
+							gameGrid[i-1][j+1] = gameGrid[i-1][j+1] + 1
+						end
+					end
+				end
+				if i < gridRows then
+					if gameGrid[i+1][j] ~= 0 then
+						gameGrid[i+1][j] = gameGrid[i+1][j] + 1
+					end
+					if j > 1 then
+						if gameGrid[i+1][j-1] ~= 0 then
+							gameGrid[i+1][j-1] = gameGrid[i+1][j-1] + 1
+						end
+					end
+					if j < gridColumns then
+						if gameGrid[i+1][j+1] ~= 0 then
+							gameGrid[i+1][j+1] = gameGrid[i+1][j+1] + 1
+						end
+					end
+				end
+				if j > 1 then
+					if gameGrid[i][j-1] ~= 0 then
+						gameGrid[i][j-1] = gameGrid[i][j-1] + 1
+					end
+				end
+				if j < gridColumns then
+					if gameGrid[i][j+1] ~= 0 then
+						gameGrid[i][j+1] = gameGrid[i][j+1] + 1
+					end
+				end
+			end
+		end
+	end
+end
+
+-- Now you can create the game grid by calling the function
+local gameGrid = createGameGrid()
+setBombs(gameGrid, nBombs)
+--fillGrid(gameGrid)
+
+-- Add this function to create the grid
+local function createGrid(sceneGroup)
+    local supbarHeight = screenH/4 -- height of the supbar
+    local gridWidth = cellSize * gridColumns -- total width of the grid
+    local xOffset = (screenW - gridWidth) / 2 + 14 -- horizontal offset to center the grid
+    for i = 1, gridRows do
+        grid[i] = {} -- create a new row
+        for j = 1, gridColumns do
+            -- create a new cell for the background color
+            local cell = display.newRect(xOffset + (j - 1) * cellSize, supbarHeight + (i - 1) * cellSize, cellSize, cellSize)
+            cell:setFillColor(0.7) 
+            cell.strokeWidth = 1 
+            cell:setStrokeColor(1, 1, 1)
+            sceneGroup:insert(cell)
+
+            -- add the images and numbers based on the gameGrid values
+            if gameGrid[i][j] == 0 then
+                local mine = display.newImageRect(resources_folder.."mine.png", cellSize, cellSize)
+                mine.x = xOffset + (j - 1) * cellSize
+                mine.y = supbarHeight + (i - 1) * cellSize
+                sceneGroup:insert(mine)
+            elseif gameGrid[i][j] > 0 then
+                local number = display.newText(gameGrid[i][j], xOffset + (j - 1) * cellSize, supbarHeight + (i - 1) * cellSize, native.systemFont, 16)
+                number:setFillColor(0, 0, 0)
+                sceneGroup:insert(number)
+            end
+
+            -- create a new cell for the image
+            local imageCell = display.newImageRect(resources_folder.."cell.jpg", cellSize, cellSize)
+            imageCell.x = xOffset + (j - 1) * cellSize
+            imageCell.y = supbarHeight + (i - 1) * cellSize
+            sceneGroup:insert(imageCell) -- add the image of the cell
+
+            -- add a touch event listener to the image cell
+            function imageCell:touch(event)
+                if event.phase == "began" then
+                    self:removeSelf() -- remove the image cell when it's clicked
+                    self = nil
+                end
+                return true
+            end
+            imageCell:addEventListener("touch", imageCell)
+
+            grid[i][j] = {cell = cell, imageCell = imageCell} -- add the cells to the grid
+        end
+    end
+end
+
+local function createCronometer(sceneGroup)
+	local clock = display.newImageRect( resources_folder.."cronometer.png", screenH/5+20, screenH/5)
+	clock.x = display.contentCenterX - (screenW/4)
+	clock.y = display.contentCenterY - (screenH/3)
+
+
+	sceneGroup:insert(clock)
+end
+
+function scene:create( event )
+    local sceneGroup = self.view
+    local background = display.newImageRect(resources_folder.."game_bg.png", screenH+200, screenH)
+    background.anchorX = 0.5
+    background.anchorY = 0.5
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
+    local game_supbar = display.newRect( display.screenOriginX, display.screenOriginY, screenW, screenH/5 )
+    game_supbar.anchorX = 0
+    game_supbar.anchorY = 0
+    game_supbar:setFillColor(unpack(sup_bar_color))
+
+	sceneGroup:insert( background )
+    sceneGroup:insert( game_supbar )
+    createGrid(sceneGroup)
+	createCronometer(sceneGroup)
+end
 
 function scene:show( event )
 	local sceneGroup = self.view
 	local phase = event.phase
-	
 	if phase == "will" then
 		-- Called when the scene is still off screen and is about to move on screen
 	elseif phase == "did" then
 		-- Called when the scene is now on screen
-		-- 
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
-		physics.start()
 	end
 end
 
 function scene:hide( event )
 	local sceneGroup = self.view
-	
 	local phase = event.phase
-	
 	if event.phase == "will" then
 		-- Called when the scene is on screen and is about to move off screen
 		--
 		-- INSERT code here to pause the scene
 		-- e.g. stop timers, stop animation, unload sounds, etc.)
-		physics.stop()
 	elseif phase == "did" then
 		-- Called when the scene is now off screen
 	end	
-	
 end
 
 function scene:destroy( event )
-
-	-- Called prior to the removal of scene's "view" (sceneGroup)
-	-- 
-	-- INSERT code here to cleanup the scene
-	-- e.g. remove display objects, remove touch listeners, save state, etc.
 	local sceneGroup = self.view
-	
-	package.loaded[physics] = nil
-	physics = nil
 end
 
 ---------------------------------------------------------------------------------
